e31cae4049b197eb529717fa1ffc7e323338193c
coovxrp <102495348+coovxrp@users.noreply.github.com>
2024-04-03 21:07:31 -0400

    update getActiveAccountNFTs.js

--- getActiveAccountNFTs.js
+++ getActiveAccountNFTs.js
@@ -57,6 +57,36 @@ function fetchAccountNFTs(account) {
   });
 }
 
+// function fetchAccountNFTs(account) {
+//   return new Promise((resolve, reject) => {
+//     ws.send(
+//       JSON.stringify({
+//         command: 'account_nfts',
+//         account,
+//         limit: 400,
+//         ledger_index: 'validated',
+//       })
+//     );
+//     ws.once('message', (data) => {
+//       const response = JSON.parse(data);
+//       if (response.status === 'success' && response.type === 'response') {
+//         resolve(response.result.account_nfts);
+//       } else if (response.error === 'actNotFound') {
+//         // Use resolve with a specific value to indicate account not found
+//         resolve('accountNotFound');
+//       } else {
+//         console.log(
+//           `Error fetching NFTs for account ${account}: ${
+//             response.error_message || 'Unknown error'
+//           }`
+//         );
+//         // Reject the promise for other types of errors
+//         reject(new Error(`Error fetching NFTs for account ${account}`));
+//       }
+//     });
+//   });
+// }
+
 async function processAccounts(
   accounts,
   accountsFilePath,
@@ -94,6 +124,44 @@ async function processAccounts(
   // Final save for any remaining processed accounts
   await saveAccountsToFile(accounts, accountsFilePath);
 }
+
+// async function processAccounts(
+//   accounts,
+//   accountsFilePath,
+//   output300MoreFilePath,
+//   output300LessFilePath
+// ) {
+//   let processedCount = 0;
+//   for (const { account, processed } of accounts) {
+//     if (processed) continue; // Skip already processed accounts
+//     try {
+//       const nfts = await fetchAccountNFTs(account);
+//       if (nfts === 'accountNotFound') {
+//         console.log(`${account} not found`);
+//         continue; // Skip this account and continue with the next one
+//       }
+//       console.log(`Processing account: ${account}, NFT count: ${nfts.length}`);
+//       if (nfts.length >= 300) {
+//         await appendAccountToCSV(account, output300MoreFilePath);
+//       } else if (nfts.length > 0) {
+//         await appendAccountToCSV(account, output300LessFilePath);
+//       }
+//       // Mark account as processed
+//       const index = accounts.findIndex((a) => a.account === account);
+//       accounts[index].processed = true;
+//       processedCount++;
+//       // Update the active accounts CSV every 100 processed accounts
+//       if (processedCount % 100 === 0) {
+//         await saveAccountsToFile(accounts, accountsFilePath);
+//       }
+//     } catch (error) {
+//       console.log(`Error processing account ${account}: ${error.message}`);
+//       // Optionally handle errors, e.g., by logging them or marking the account as errored
+//     }
+//   }
+//   // Final save for any remaining processed accounts
+//   await saveAccountsToFile(accounts, accountsFilePath);
+// }
 
 ws.on('open', async () => {
   console.log('Connected to XRPL WebSocket server');
